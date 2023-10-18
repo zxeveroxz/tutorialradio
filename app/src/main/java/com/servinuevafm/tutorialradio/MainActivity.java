cc2684c2b29c763aafb16efc94b2da52a6368281
zxeveroxz <s.zegarra@hotmail.com>
2023-10-17 23:49:22 -0500

    primer commit 2

--- app/src/main/java/com/servinuevafm/tutorialradio/MainActivity.java
+++ app/src/main/java/com/servinuevafm/tutorialradio/MainActivity.java
@@ -3,53 +3,37 @@ package com.servinuevafm.tutorialradio;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
-import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 
-import android.os.Handler;
+
 import android.view.View;
-import android.view.WindowManager;
+
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Switch;
 import android.widget.Toast;
 
-
-import com.chibde.visualizer.BarVisualizer;
-import com.chibde.visualizer.LineVisualizer;
 import com.chibde.visualizer.SquareBarVisualizer;
-import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.MediaItem;
-import com.google.android.exoplayer2.Player;
+
 
 
 public class MainActivity extends AppCompatActivity {
-    private WebView webView;
     private ExoPlayer exoPlayer;
-    private Uri streamUri;
-
-    public MediaPlayer mediaPlayer;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        webView = findViewById(R.id.webView);
+        WebView webView = findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
-        webView.getSettings().setJavaScriptEnabled(true);
+        //webView.getSettings().setJavaScriptEnabled(true);
         webView.loadUrl("https://radiofelima.radio12345.com/");
 
         exoPlayer = new ExoPlayer.Builder(this).build();
         Switch switchStreaming = findViewById(R.id.switchStreaming);
-
-
-//playing media
-        //mediaPlayer.start();
-
 
 
         switchStreaming.setOnCheckedChangeListener((buttonView, isChecked) -> {
